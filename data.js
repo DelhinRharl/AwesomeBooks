2f3220c25bd1d335dadfc64622c80e5939bb8d77
abdollah raafat <abdollah.raafat.daif@gmail.com>
2021-11-30 12:54:53 +0200

    Fix function to be arrow function

--- data.js
+++ data.js
@@ -1,15 +1,15 @@
-let Title;
-let Author;
+
+let Title, Author; 
 let bookList = [];
 
-function getValues() {
+getValues = () => {
   Title = document.querySelector('#title').value;
   Author = document.querySelector('#author').value;
   document.querySelector('#title').value = '';
   document.querySelector('#author').value = '';
 }
 
-function addBook() {
+  addBook = () => {
   document.getElementById('dynamic').innerHTML = '';
   bookList.forEach((book, index) => {
     document.getElementById('dynamic').innerHTML += `
@@ -18,30 +18,32 @@ function addBook() {
       <p class="authir">${book.author}</p>
       <button class="remove" onClick = 'removeItem(${index})'>Remove</button>
       <hr> 
-    </div>`;
-  });
+    </div>`
+  })
 }
-if (localStorage.getItem('Books')) {
+if(localStorage.getItem('Books')) {
   bookList = JSON.parse(localStorage.getItem('Books'));
   addBook();
 }
 
-function storeBook() {
+ storeBook = () => {
+ 
   getValues();
-  const data = {
-    title: Title,
-    author: Author,
-  };
+  let data = {
+    title:  Title,
+    author: Author
+  }
   bookList.push(data);
   Title = '';
   Author = '';
   localStorage.setItem('Books', JSON.stringify(bookList));
   addBook();
 }
-/* eslint-disable no-unused-vars */
-const btnAdd = document.getElementById('btn');
+
+let btnAdd= document.getElementById("btn");
+
 btnAdd.addEventListener('click', storeBook);
-function removeItem(index) {
+removeItem = (index) => {
   bookList.splice(index, 1);
   localStorage.setItem('Books', JSON.stringify(bookList));
   addBook();
