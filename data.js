a95dc4fc037c8a8d332a1d668d4533ef98cf222b
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2021-12-01 09:23:31 +0100

    Refactoring using classes

--- data.js
+++ data.js
@@ -8,69 +8,33 @@ class Book {
 class BookL {
   bookList = [];
   add() {
+    let Title;
+    let Author;
     Title = document.querySelector('#title').value;
     Author = document.querySelector('#author').value;
     if (Title !=='' && Author !=='') {
       let book = new Book(Title,Author);
       this.bookList.push(book);
-      console.log(this.bookList);
-      document.getElementById('dynamic').innerHTML = '';
-      this.bookList.forEach((book, index) => {
-        document.getElementById('dynamic').innerHTML += `
-        <div class="books">
-          <p class="title">${book.Title}</p>
-          <p class="authir">${book.Author}</p>
-          <button class="remove" onClick = 'removeItem(${index})'>Remove</button>
-          <hr>
-        </div>`;
-      });
+      this.render();
     }
   }
-}
-
-let Title;
-let Author;
-
-
-const getValues = () => {
-  Title = document.querySelector('#title').value;
-  Author = document.querySelector('#author').value;
-  document.querySelector('#title').value = '';
-  document.querySelector('#author').value = '';
-};
-
-const addBook = () => {
-  document.getElementById('dynamic').innerHTML = '';
-  bookList.forEach((book, index) => {
-    document.getElementById('dynamic').innerHTML += `
-    <div class="books">
-      <p class="title">${book.title}</p>
-      <p class="authir">${book.author}</p>
-      <button class="remove" onClick = 'removeItem(${index})'>Remove</button>
-      <hr>
-    </div>`;
-  });
-};
-
-if (localStorage.getItem('Books')) {
-  bookList = JSON.parse(localStorage.getItem('Books'));
-  addBook();
-}
-
-const storeBook = () => {
-  getValues();
-  const data = {
-    title: Title,
-    author: Author,
-  };
-  if (data.title !== '' && data.author !== '') {
-    bookList.push(data);
+  render() {
+    document.getElementById('dynamic').innerHTML = '';
+    this.bookList.forEach((book, index) => {
+      document.getElementById('dynamic').innerHTML += `
+      <div class="books">
+        <p class="title">${book.Title}</p>
+        <p class="authir">${book.Author}</p>
+        <button class="remove" onClick = 'bookl.remove(${index})'>Remove</button>
+        <hr>
+      </div>`;
+    });
   }
-  Title = '';
-  Author = '';
-  localStorage.setItem('Books', JSON.stringify(bookList));
-  addBook();
-};
+  remove(index) {
+    this.bookList.splice(index, 1);
+    this.render();
+  }
+}
 
 
 let bookl = new BookL();
@@ -78,12 +42,6 @@ document.getElementById('btn').addEventListener('click', function(){
 bookl.add();
 })
 //btnAdd.addEventListener('click', storeBook);
-
-const removeItem = (index) => {
-  bookList.splice(index, 1);
-  localStorage.setItem('Books', JSON.stringify(bookList));
-  addBook();
-};
 
 if (1 === 0) {
   removeItem();
